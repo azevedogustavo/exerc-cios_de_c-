a7dffffc421c64b15d9910e32e0ec668ac379323
Gustavo Azevedo <gustavo.azevedo@stone.com.br>
2021-01-12 17:56:47 -0300

    fixed program

--- birthday-cake-candles/birthday-cake-candles.cpp
+++ birthday-cake-candles/birthday-cake-candles.cpp
@@ -1,45 +1,88 @@
-#include <iostream>
-// esse código possui um array já instanciado
-// como o programa é pequeno, não achei necessário separar as funções em um outro arquivo
-int max_value(int *candles, int size);
-int repeated_quantities(int *candles, int size, int val_max);
+#include <bits/stdc++.h>
 
-int main(){
-    int array[] = {10,4,6,4};
-    std::cout << "os maiores valores das 'velas' e suas contidades repetidas em um array \n";
-    int val_max{};
-    int val_max_repeat{};
-    int array_size=sizeof(array) / sizeof(array[0]);
-    // lembrar que o comprimento do array é definido pelo seu tamanho em memória dividio por um 
-    // componente do array. EX: sizeof(array) / sizeof(array[0])
-    val_max = max_value(array, array_size);
-    val_max_repeat = repeated_quantities(array, array_size, val_max);
-     
-     if (val_max_repeat > 1)
-     std::cout << "os valore da velas mais altas é " << val_max << " com " 
-     << val_max_repeat << " repeticoes ";
-     if (val_max_repeat == 1)
-    std::cout << "os valore da velas mais altas é " << val_max << " com " 
-    << val_max_repeat << " repeticao ";
-     else
-     std::cout << "os valore da velas mais altas é " << val_max << " sem repeticao \n";
-    //std::cout<< sizeof(candles);
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+int birthdayCakeCandles(vector<int> candles) {
+
 }
 
-int max_value(int *candles, int size){
-    int val_max{};
-    for(int i=0; i < sizeof(candles) / sizeof(candles[0]); i++){
-         //std::cout<< candles[i] << " ";
-         if (val_max < candles[i])
-         val_max=candles[i];
-         }
-         return val_max;
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string candles_count_temp;
+    getline(cin, candles_count_temp);
+
+    int candles_count = stoi(ltrim(rtrim(candles_count_temp)));
+
+    string candles_temp_temp;
+    getline(cin, candles_temp_temp);
+
+    vector<string> candles_temp = split(rtrim(candles_temp_temp));
+
+    vector<int> candles(candles_count);
+
+    for (int i = 0; i < candles_count; i++) {
+        int candles_item = stoi(candles_temp[i]);
+
+        candles[i] = candles_item;
+    }
+
+    int result = birthdayCakeCandles(candles);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
 }
-int repeated_quantities(int *candles, int size, int val_max){
-    int val_max_repeat={};
-for(int i=0; i < sizeof(candles) / sizeof(candles[0]); i++){
-         if(val_max==candles[i])
-         val_max_repeat++;
-     }
-     return val_max_repeat;
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
